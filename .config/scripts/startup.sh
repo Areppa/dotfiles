145becfabf9aca8c4e06b88061683e1c1d733104
Areppa <aaro.halme2000@gmail.com>
2025-10-06 03:47:35 +0300

    Added startup script

---
+++ .config/scripts/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# TODO
+# - Run ansible playbook on the system automatically on startup
+
+# Remove specific flatpaks automatically
+flatpak remove org.gnome.Mines org.gnome.Mahjongg -y
+
+# Upgrade flatpaks
+flatpak upgrade -y
