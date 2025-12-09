0e8b9e451f1af57d1e39c546b42fdd802c248c01
Areppa <aaro.halme2000@gmail.com>
2025-12-09 11:33:47 +0200

    List pacman apps to a file

--- .config/scripts/maintenance.sh
+++ .config/scripts/maintenance.sh
@@ -5,3 +5,7 @@ source ~/.config/scripts/startup.sh
 
 # Update Flatpaks
 flatpak update -y
+
+# List user installed pacman apps and save it to txt file
+pacman -Qqe > ~/Programs/Backup/pacman/$HOSTNAME\_apps.txt
+echo Saved apps list to: $HOSTNAME\_apps.txt
