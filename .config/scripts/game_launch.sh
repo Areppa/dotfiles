b78827a60fcc7ce60971cc7e3e577b3cbeb1ab3d
Areppa <aaro.halme2000@gmail.com>
2026-01-13 11:44:43 +0200

    Added notify and variable for time

--- .config/scripts/game_launch.sh
+++ .config/scripts/game_launch.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 SCRIPT=$HOME/dotfiles/.config/scripts/toggle_secondary_monitor.sh
+TIME=30
 
 echo "Disabling 2nd display..."
+notify-send "Disabling 2nd display for $TIME seconds"
 $SCRIPT 0
 
-sleep 30
+sleep $TIME
 
 echo "Enabling 2nd display..."
 $SCRIPT 1
