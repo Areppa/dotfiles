ecc1476c1b49c9f4742312fc2d828c6acd8a3c2f
Areppa <aaro.halme2000@gmail.com>
2026-01-23 04:41:22 +0200

    Integrated monitor switching to the script

--- .config/scripts/game_launch.sh
+++ .config/scripts/game_launch.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-SCRIPT=$HOME/dotfiles/.config/scripts/toggle_secondary_monitor.sh
-TIME=30
+# This script disables 2nd monitor temporarily
+# so that games can launch with correct resolution
+
+# Monitor to disable temporarily
+MON="DVI-D-1"
+# Monitor config: resolution@refreshrate,location,scale
+MON_CFG=1680x1050@59.95,1920x0,1
+# Time to disable second monitor
+TIME=5
 
 echo "Disabling 2nd display..."
-notify-send "Disabling 2nd display for $TIME seconds"
-$SCRIPT 0
+notify-send "Disabling $MON temporarily" "It will turn on in $TIME seconds"
+hyprctl keyword monitor "${MON}, disable"
 
 sleep $TIME
 
-echo "Enabling 2nd display..."
-$SCRIPT 1
+echo "Enabling $MON"
+hyprctl keyword monitor "${MON}, ${MON_CFG}"
