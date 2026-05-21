cb91fcacffce316365fd3222f60299f7210630ab
Areppa <aaro.halme2000@gmail.com>
2026-05-21 08:24:23 +0300

    Added pacman maintenance (requires root)

--- .config/scripts/maintenance.sh
+++ .config/scripts/maintenance.sh
@@ -9,3 +9,9 @@ flatpak update -y
 # List user installed pacman apps and save it to txt file
 pacman -Qqe > ~/Programs/Backup/pacman/$HOSTNAME\_apps.txt
 echo Saved apps list to: $HOSTNAME\_apps.txt
+
+echo #Downloading updates without applying them
+sudo pacman -Syyuw --noconfirm
+
+echo #Removing orphaned packages
+sudo pacman -Rns $(pacman -Qdtq)
