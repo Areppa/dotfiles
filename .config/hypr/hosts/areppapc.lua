8f016b03e40af378d3b988bec7f19ae9539eeedb
Areppa <aaro.halme2000@gmail.com>
2026-08-13 19:57:16 +0300

    Added workspace rules for areppapc

--- .config/hypr/hosts/areppapc.lua
+++ .config/hypr/hosts/areppapc.lua
@@ -8,3 +8,32 @@ hl.monitor({
     position = "1920x0",
     scale = "1",
 })
+
+-----------------------
+--- WORKSPACE RULES ---
+-----------------------
+
+hl.workspace_rule({
+    workspace = 1,
+    monitor = "DVI-D-1",
+})
+
+hl.workspace_rule({
+    workspace = 2,
+    monitor = "DVI-D-1",
+})
+
+hl.workspace_rule({
+    workspace = 3,
+    monitor = "DVI-D-1",
+})
+
+hl.workspace_rule({
+    workspace = 5,
+    monitor = "DP-2",
+})
+
+hl.workspace_rule({
+    workspace = 10,
+    monitor = "DP-2",
+})
